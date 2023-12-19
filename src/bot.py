9bddd9e9e94f6c622c284e2c1e2bf0a585e0db2a
Erik Wessman <erikwessman10@gmail.com>
2023-12-19 16:29:31 +0100

    Switched to using cogs, attempt

--- src/bot.py
+++ src/bot.py
@@ -1,4 +1,5 @@
 import os
+import asyncio
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
@@ -8,16 +9,9 @@ import helper
 from const import CHANNEL_ID, GITHUB_REPOSITORY, GITHUB_ICON
 
 
-load_dotenv()
-
-
 class WaPoBot(commands.Bot):
     def __init__(self, command_prefix, intents):
         super().__init__(command_prefix=command_prefix, intents=intents)
-        self.is_generating_url = False
-        self.players = {}
-        self.solved = set()
-        self.add_commands()
 
     async def on_ready(self):
         print(f"{self.user} has connected!")
@@ -25,60 +19,61 @@ class WaPoBot(commands.Bot):
     async def on_command_error(self, ctx, error):
         print(f"Command error: {error}")
 
-    def add_commands(self):
-        @self.command(name="wapo", pass_context=True)
-        async def wapo(ctx):
-            if CHANNEL_ID == ctx.channel.id and not self.is_generating_url:
-                embed_loading = discord.Embed(
-                    title="Washington Post Daily Crossword",
-                    description="Fetching URL...",
-                    color=discord.Color.teal(),
+
+class WaPoBotCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.is_generating_url = False
+        self.players = {}
+        self.solved = set()
+
+    @commands.command()
+    async def wapo(self, ctx):
+        if CHANNEL_ID == ctx.channel.id and not self.is_generating_url:
+            embed_loading = discord.Embed(
+                title="Washington Post Daily Crossword",
+                description="Fetching URL...",
+                color=discord.Color.teal(),
+            )
+            embed_loading.set_footer(text=GITHUB_REPOSITORY, icon_url=GITHUB_ICON)
+            message = await ctx.send(embed=embed_loading)
+
+            try:
+                self.is_generating_url = True
+
+                url = wapo_api.get_wapo_url()
+                date_str = helper.get_puzzle_date(url)
+                weekday_str = helper.get_puzzle_weekday(date_str)
+
+                embed_success = embed_loading.copy()
+                embed_success.url = url
+                embed_success.description = (
+                    f"URL for {weekday_str} generated! ({date_str})"
                 )
-                embed_loading.set_footer(text=GITHUB_REPOSITORY, icon_url=GITHUB_ICON)
-                message = await ctx.send(embed=embed_loading)
+                embed_success.color = discord.Color.green()
+                await message.edit(embed=embed_success)
 
-                try:
-                    self.is_generating_url = True
+            except Exception as e:
+                embed_error = embed_loading.copy()
+                embed_error.description = "Error fetching URL"
+                embed_error.color = discord.Color.red()
+                await message.edit(embed=embed_error)
 
-                    url = wapo_api.get_wapo_url()
-                    date_str = helper.get_puzzle_date(url)
-                    weekday_str = helper.get_puzzle_weekday(date_str)
+                print(f"Unable to get URL: {e}")
 
-                    embed_success = embed_loading.copy()
-                    embed_success.url = url
-                    embed_success.description = (
-                        f"URL for {weekday_str} generated! ({date_str})"
-                    )
-                    embed_success.color = discord.Color.green()
-                    await message.edit(embed=embed_success)
-
-                except Exception as e:
-                    embed_error = embed_loading.copy()
-                    embed_error.description = "Error fetching URL"
-                    embed_error.color = discord.Color.red()
-                    await message.edit(embed=embed_error)
-
-                    print(f"Unable to get URL: {e}")
-
-                finally:
-                    self.is_generating_url = False
+            finally:
+                self.is_generating_url = False
 
     @commands.Cog.listener()
     async def on_reaction_add(self, reaction, user):
-        if user == self.user:
+        if user == self.bot.user:
             return
-
-        print("user reacted to a message")
 
         if reaction.message.channel.id != CHANNEL_ID:
             return
 
-        print("correct channel")
-
         if reaction.emoji != "👍" and reaction.emoji != "✅":
             return
-
-        print("correct emoji")
 
         if len(reaction.embeds) == 0:
             return
@@ -121,13 +116,16 @@ class WaPoBot(commands.Bot):
         )
 
 
-def main():
+async def main():
     intents = discord.Intents.default()
     intents.message_content = True
+    intents.reactions = True
 
     bot = WaPoBot(command_prefix="!", intents=intents)
-    bot.run(os.getenv("DISCORD_TOKEN"))
+    await bot.add_cog(WaPoBotCog(bot))
+    await bot.start(os.getenv("DISCORD_TOKEN"))
 
 
 if __name__ == "__main__":
-    main()
+    load_dotenv()
+    asyncio.run(main())
