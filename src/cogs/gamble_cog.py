384558e48eb78b3393fc8785b6eeedfc59d2d757
Erik Wessman <erikwessman10@gmail.com>
2024-01-04 23:02:23 +0100

    Fixed issue with roulette winner

--- src/cogs/gamble_cog.py
+++ src/cogs/gamble_cog.py
@@ -160,7 +160,7 @@ class GambleCog(commands.Cog):
         user_coins = [user_info["coins"] for user_info in participants.values()]
 
         winner = random.choices(users, weights=user_coins, k=1)[0]
-        winner_player = self.bot.player_service.get_player(winner)
+        winner_player = self.bot.player_service.get_player(winner.id)
         win_amount = sum(user_coins)
 
         odds_table = get_odds_table(participants)
