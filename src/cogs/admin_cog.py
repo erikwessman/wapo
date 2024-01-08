73e7679fe2785bff3555219e31576b7fa30bec3f
theowiik <theo.le.wiik@gmail.com>
2024-01-08 13:42:01 +0100

    Add dev-ping command to AdminCog

--- src/cogs/admin_cog.py
+++ src/cogs/admin_cog.py
@@ -1,10 +1,24 @@
+import os
 from discord.ext import commands
 
 
 class AdminCog(commands.Cog):
+    _NOT_DEV_MODE_MSG = "This command is only available in dev mode"
+
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.hybrid_command(name="helloworld", description="Says hello to the world")
-    async def helloworld(self, ctx: commands.Context):
-        await ctx.send(embed="hello world", ephemeral=True)
+    def is_dev_mode(self):
+        dev_mode = os.getenv("DEV_MODE")
+        if dev_mode is None:
+            return False
+
+        return dev_mode.lower() == "true"
+
+    @commands.hybrid_command(name="dev-ping", description="Ping, pong!")
+    async def dev_ping(self, ctx: commands.Context):
+        if not self.is_dev_mode():
+            await ctx.send(content=self._NOT_DEV_MODE_MSG, ephemeral=True)
+            return
+
+        await ctx.send(content="Pong!", ephemeral=True)
