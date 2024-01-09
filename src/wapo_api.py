062692dbac7c9d8d32491dd0772cbd2dba5a09e3
Erik Wessman <erikwessman10@gmail.com>
2024-01-09 20:16:23 +0100

    .

--- src/wapo_api.py
+++ src/wapo_api.py
@@ -141,7 +141,7 @@ def get_puzzle_time(url: str) -> int:
         )
         driver.switch_to.frame(crossword_frame)
 
-        driver.save_full_page_screenshot("my_screenshot.png")
+        driver.save_screenshot("my_screenshot.png")
 
         time_str = wait.until(
             EC.visibility_of_element_located((By.ID, "clock_str"))
