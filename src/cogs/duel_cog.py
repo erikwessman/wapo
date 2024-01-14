ede9568d88636fc73de402526308022dd2b508ee
Erik Wessman <erikwessman10@gmail.com>
2024-01-14 18:04:10 +0100

    Fixed if user is none

--- src/cogs/duel_cog.py
+++ src/cogs/duel_cog.py
@@ -187,8 +187,8 @@ class DuelCog(commands.Cog):
 
     @duel.command(name="start")
     async def duel_start(self, ctx: commands.Context, user: discord.User, amount: int):
-        if ctx.author.id == user.id:
-            raise commands.CommandError("Can't challenge yourself")
+        #if ctx.author.id == user.id:
+        #    raise commands.CommandError("Can't challenge yourself")
 
         if amount < 1:
             raise commands.CommandError("Must wager at least 1 coin")
@@ -238,7 +238,7 @@ class DuelCog(commands.Cog):
 
     @duel.command(name="accept")
     async def duel_accept(self, ctx: commands.Context, user: discord.User = None):
-        if ctx.author.id == user.id:
+        if user and ctx.author.id == user.id:
             raise commands.CommandError("Can't accept a duel from yourself")
 
         player = self.bot.player_service.get_player(ctx.author.id)
